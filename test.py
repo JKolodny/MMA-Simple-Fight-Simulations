4392679dad310c6551019f786b5cd1fb4c499bad
Joshua Kolodny <kolo.josh@gmail.com>
2023-09-01 23:45:21 -0400

    test

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
 import numpy as np
 
-x = [1, 2, 3]
+LIFES = 30
+SIMULATIONS = 650
 
-y = np.mean(x)
+life_value = []
+for _ in range(LIFES):
+    life_time_value = 0
+    for _ in range(SIMULATIONS):
+        AT_BAT = np.random.randint(0, 10)
+        if AT_BAT > 7:
+            life_time_value += 1
 
-print(y, x, y)
+    life_value.append(life_time_value)
+
+MIN = min(life_value)
+MAX = max(life_value)
+MEAN = int(np.mean(life_value))
+
+
+print("MIN:", MIN)
+print("MEAN:", MEAN)
+print("MAX:", MAX)
+print("MAX - MIN:", MAX - MIN)
+
+
